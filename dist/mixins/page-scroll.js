ad6e238a01ac3d6358aded8655c1e5bbfcb04e3e
wzs28150 <wz666s@163.com>
2020-05-04 19:22:42 +0800

    add

---
+++ dist/mixins/page-scroll.js
@@ -0,0 +1,31 @@
+function getCurrentPage() {
+    const pages = getCurrentPages();
+    return pages[pages.length - 1] || {};
+}
+function onPageScroll(event) {
+    const { vanPageScroller = [] } = getCurrentPage();
+    vanPageScroller.forEach((scroller) => {
+        if (typeof scroller === 'function') {
+            scroller(event);
+        }
+    });
+}
+export const pageScrollMixin = (scroller) => Behavior({
+    attached() {
+        const page = getCurrentPage();
+        if (Array.isArray(page.vanPageScroller)) {
+            page.vanPageScroller.push(scroller.bind(this));
+        }
+        else {
+            page.vanPageScroller = [
+                page.onPageScroll.bind(page),
+                scroller.bind(this)
+            ];
+        }
+        page.onPageScroll = onPageScroll;
+    },
+    detached() {
+        const page = getCurrentPage();
+        page.vanPageScroller = (page.vanPageScroller || []).filter(item => item !== scroller);
+    }
+});
