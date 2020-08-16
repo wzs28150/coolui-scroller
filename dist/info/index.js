79417ea51bd2d2f1ab2754c5c0393614b38b5a08
wzs28150 <wz666s@163.com>
2020-08-16 10:58:34 +0800

    删除多余的测试整合文件夹

--- dist/info/index.js
+++
@@ -1,8 +0,0 @@
-import { CooluiComponent } from '../common/component';
-CooluiComponent({
-    props: {
-        dot: Boolean,
-        info: null,
-        customStyle: String,
-    },
-});
--- miniprogram_npm/coolui_scroll/common/component.d.ts
+++
@@ -1,3 +0,0 @@
-import { CooluiComponentOptions, CombinedComponentInstance } from 'definitions/index';
-declare function CooluiComponent<Data, Props, Methods>(vantOptions?: CooluiComponentOptions<Data, Props, Methods, CombinedComponentInstance<Data, Props, Methods>>): void;
-export { CooluiComponent };
