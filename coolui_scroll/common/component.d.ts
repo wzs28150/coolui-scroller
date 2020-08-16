2c61f53c0855b86919ae3595543f383f9ae2470b
wzs28150 <wz666s@163.com>
2020-08-16 10:36:41 +0800

    删除多余的测试整合文件夹

---
+++ coolui_scroll/common/component.d.ts
@@ -0,0 +1,3 @@
+import { CooluiComponentOptions, CombinedComponentInstance } from 'definitions/index';
+declare function CooluiComponent<Data, Props, Methods>(vantOptions?: CooluiComponentOptions<Data, Props, Methods, CombinedComponentInstance<Data, Props, Methods>>): void;
+export { CooluiComponent };
